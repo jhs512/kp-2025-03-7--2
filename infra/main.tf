3bb982d51a8653689be0b0004a3f39c74a40e535
jhs512 <jangka512@gmail.com>
2025-03-18 11:22:53 +0900

    work

--- infra/main.tf
+++ infra/main.tf
@@ -250,7 +250,7 @@ CREATE DATABASE glog_prod;
 FLUSH PRIVILEGES;
 "
 
-echo "${var.github_access_token_1}" | docker login ghcr.io -u jhs512 --password-stdin
+echo "${var.github_access_token_1}" | docker login ghcr.io -u ${var.github_access_token_1_owner} --password-stdin
 
 END_OF_FILE
 }
